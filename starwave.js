4c3c9d7a0e358bd40017d616f8e9927ea771f419
Peter Stuifzand <peter.stuifzand@gmail.com>
2009-06-09 17:40:28 +0200

    OO code + demo mode

--- starwave.js
+++ starwave.js
@@ -1,27 +1,3 @@
-var bullets = [];
-var enemies = [];
-
-var x = 0;
-var y = 0;
-
-var bullet_count = 100;
-var prevx = 0;
-var prevy = 0;
-
-var lives = 3;
-var score = 0;
-
-var dead_enemies = 0;
-var level = 0;
-
-var widths = [ 640, 800, 1024, 1280, 1600, 1920 ];
-
-var ox = 0;
-var oy = 0;
-
-var paused = true;
-var targetinglaser = true;
-
 function rnd(min,max) {
     return min+Math.floor(Math.random()*(max-min))
 }
@@ -29,39 +5,100 @@ function rndf(min,max) {
     return min+Math.random()*(max-min);
 }
 
-function enemy_generator() {
-    var randomnumber=rnd(20,widths[level]-20);
+function Starwave(obj, callbacks) {
+    // Callbacks
+    this.callbacks = callbacks;
 
-    var en = { x: randomnumber, y: 0, size: rnd(6,15) };
+    // CanvasElement
+    this.obj = obj;
+
+    // Objects
+    this.bullets = [];
+    this.enemies = [];
+
+    // Player
+    this.x = 0;
+    this.y = 0;
+
+	this.bullet_count = 100;
+	this.prevx = 0;
+	this.prevy = 0;
+
+	this.lives = 3;
+	this.score = 0;
+
+	this.dead_enemies = 0;
+	this.level = 1;
+
+	this.widths = [ 640, 800, 1024, 1280, 1600, 1920 ];
+
+	this.ox = 0;
+	this.oy = 0;
+
+	this.paused = true;
+    this.demo = true;
+	this.targetinglaser = true;
+
+    this.obj.width = this.widths[this.level - 1];
+    this.ox = this.obj.offsetLeft;
+    this.oy = this.obj.offsetTop;
+
+    var ctx = this.obj.getContext('2d');
+
+    var game = this;
+    obj.onmousedown = function(e) {
+        if (!game.paused) {
+            game.bullets.push({ size: 3, x: e.clientX - game.ox, y: 460 });
+            game.bullet_count -= 1;
+            game.callbacks.shot_fired(game);
+        }
+    };
+
+    obj.onmousemove = function(e) {
+        game.x = e.clientX - game.ox;
+        game.y = e.clientY - game.oy;
+    };
+}
+
+Starwave.prototype.enemy_generator = function() {
+    var ex = rnd(20,this.widths[this.level - 1]-20);
+
+    var en = { x: ex, y: 0, size: rnd(6,15) };
     en.speed = 0.5 + (2*en.size) / 15;
-    enemies.push(en);
-    if (!paused) {
-        setTimeout('enemy_generator()', rnd(800, 1000));
+
+    this.enemies.push(en);
+
+    if (!this.paused) {
+        setTimeout('game.enemy_generator()', rnd(800, 1000));
+    }
+    if (this.demo) {
+        setTimeout('game.enemy_generator()', 50);
     }
 }
 
-function object_mover() {
-    var obj = document.getElementById('starwave');
-    var ctx = obj.getContext('2d');
+
+Starwave.prototype.object_mover = function () {
+    var ctx = this.obj.getContext('2d');
 
     var grad = ctx.createLinearGradient(0, 0, 0, 1500);
     grad.addColorStop(1.0,'#0000ff');
     grad.addColorStop(0.0, '#000000');
-    ctx.fillStyle = grad;
-    ctx.fillRect(0, 0, widths[level], 440);
-    ctx.fillStyle = '#00ff00';
-    ctx.fillRect(0, 440, widths[level], 40);
 
-    y = 460;
+    ctx.fillStyle = grad;
+    ctx.fillRect(0, 0, this.widths[this.level - 1], 440);
+    ctx.fillStyle = '#00ff00';
+    ctx.fillRect(0, 440, this.widths[this.level - 1], 40);
+
+    this.y = 460;
 
     var width = 10;
     var height = 20;
 
-    for (var i in enemies) {
-        var enemy = enemies[i];
+    for (var i in this.enemies) {
+        var enemy = this.enemies[i];
 
-        for (var j in bullets) {
-            var bullet = bullets[j];
+        for (var j in this.bullets) {
+            var bullet = this.bullets[j];
             
             var dx = enemy.x - bullet.x;
             var dy = enemy.y - bullet.y;
@@ -71,32 +108,33 @@ function object_mover() {
                 bullet.dead=true;
                 enemy.dead=true;
 
-                score += 10;
-                dead_enemies += 1;
+                this.score += 10;
+                this.dead_enemies += 1;
 
-                if (dead_enemies >= level*10) {
-                    dead_enemies = 0;
-                    level++;
-                    if (level >= widths.length) {
-                        level = widths.length-1;
+                if (this.dead_enemies >= this.level*10) {
+                    this.dead_enemies = 0;
+                    this.level++;
+                    if (this.level > this.widths.length) {
+                        this.level = this.widths.length;
                     }
                     else {
-                        obj.width = widths[level];
+                        this.obj.width = this.widths[this.level - 1];
                     }
 
-                    $('#level').html(level);
 
-                    ox = obj.offsetLeft;
-                    oy = obj.offsetTop;
+                    this.callbacks.level_changed(this);
+
+
+                    this.ox = this.obj.offsetLeft;
+                    this.oy = this.obj.offsetTop;
                 }
-                $('#score').html(score);
-                console.log(score);
+                $('#score').html(this.score);
             }
         }
     }
 
-    for (var i in bullets) {
-        var obj = bullets[i];
+    for (var i in this.bullets) {
+        var obj = this.bullets[i];
 
         ctx.beginPath();
         ctx.fillStyle = '#ffffff';
@@ -110,127 +148,104 @@ function object_mover() {
         }
     }
 
-    for (var i in bullets.reverse()) {
-        var obj = bullets[i];
+    for (var i in this.bullets.reverse()) {
+        var obj = this.bullets[i];
         if (obj.dead) {
-            bullets.splice(i, 1);
+            this.bullets.splice(i, 1);
         }
     }
 
-    for (var i in bullets) {
-        var obj = bullets[i];
-
-        /*
-        ctx.beginPath();
-        ctx.fillStyle = '#000000';
-        ctx.arc(obj.x, obj.y, obj.size, 0, 360, false);
-        ctx.fill();
-        ctx.closePath();
-        */
-    }
-
-    for (var i in enemies) {
-        var obj = enemies[i];
+    for (var i in this.enemies) {
+        var obj = this.enemies[i];
 
         obj.y = obj.y + obj.speed;
         if (obj.y > 480) {
             obj.dead=true;
-            lives -= 1;
-            $('#lives').html(lives);
-            console.log(lives);
+
+            if (!this.paused) {
+                this.lives -= 1;
+                $('#lives').html(this.lives);
+            }
         }
     }
 
-    for (var i in enemies.reverse()) {
-        var obj = enemies[i];
+    for (var i in this.enemies.reverse()) {
+        var obj = this.enemies[i];
         if (obj.dead) {
-            enemies.splice(i, 1);
+            this.enemies.splice(i, 1);
         }
     }
 
-    for (var i in enemies) {
-        var obj = enemies[i];
+    for (var i in this.enemies) {
+        var obj = this.enemies[i];
 
         ctx.beginPath();
         ctx.fillStyle = '#ff0000';
         ctx.arc(obj.x, obj.y, obj.size, 0, 360, false);
         ctx.fill();
         ctx.closePath();
+    }
+
+    this.prevy=460;
+    if (!this.demo) {
         ctx.beginPath();
-        ctx.strokeStyle = '#ffffff';
-        ctx.arc(obj.x, obj.y, obj.size+5, 0, 360, false);
-        ctx.stroke();
+        ctx.fillStyle = '#000000';
+        ctx.moveTo(this.x, this.y-height);
+        ctx.lineTo(this.x+width, this.y+height);
+        ctx.lineTo(this.x-width, this.y+height);
+        ctx.lineTo(this.x, this.y-height);
+        ctx.fill();
         ctx.closePath();
     }
 
-    prevy=460;
-    /*
-    ctx.beginPath();
-    ctx.fillStyle = '#ffffff';
-    ctx.moveTo(prevx, prevy-height-2);
-    ctx.lineTo(prevx+width+2, prevy+height+2);
-    ctx.lineTo(prevx-width-2, prevy+height+2);
-    ctx.lineTo(prevx, prevy-height-2);
-    ctx.fill();
-    ctx.closePath();
-    */
-
-    ctx.beginPath();
-    ctx.fillStyle = '#000000';
-    ctx.moveTo(x, y-height);
-    ctx.lineTo(x+width, y+height);
-    ctx.lineTo(x-width, y+height);
-    ctx.lineTo(x, y-height);
-    ctx.fill();
-    ctx.closePath();
-
-    if (targetinglaser) {
+    if (!this.demo && this.targetinglaser) {
         ctx.beginPath();
         ctx.strokeStyle = '#ffff00';
-        ctx.moveTo(x, y-height);
-        ctx.lineTo(x, 0);
+        ctx.moveTo(this.x, this.y-height);
+        ctx.lineTo(this.x, 0);
         ctx.stroke();
         ctx.closePath();
     }
 
-    prevx = x;
-    prevy = 460;
+    this.prevx = this.x;
+    this.prevy = 460;
 
-    if (ctx.fillText) {
-        ctx.fillText("Level " + level, 10, 10);
+    if (!this.paused) {
+        setTimeout('game.object_mover()', 10);
     }
-
-    if (!paused) {
-        setTimeout('object_mover()', 10);
+    if (this.demo) {
+        setTimeout('game.object_mover()', 10);
     }
 }
 
+Starwave.prototype.toggle_pause = function() {
+    this.paused = !this.paused;
+
+    if (!this.demo) {
+        if (!this.paused) {
+            this.object_mover();
+            this.enemy_generator();
+        }
+    }
+    this.demo = false;
+}
+
+var game;
+
 $(document).ready(function() {
     var obj = document.getElementById('starwave');
-    obj.width = widths[level];
-    ox = obj.offsetLeft;
-    oy = obj.offsetTop;
-    var ctx = obj.getContext('2d');
 
-    obj.onmousedown = function(e) {
-        if (!paused) {
-            bullets.push({ size: 3, x:e.clientX - ox, y:460 });
-            bullet_count -= 1;
-            $('#bullets').html(bullet_count);
-        }
-    };
-
-
-    obj.onmousemove = function(e) {
-        x = e.clientX - ox;
-        y = e.clientY - oy;
-    };
+    game = new Starwave(obj, {
+        shot_fired:    function(game) { $('#bullets').html(game.bullet_count) },
+        level_changed: function(game) { $('#level').html(game.level) }
+    });
+    game.object_mover();
+    game.enemy_generator();
 });
 
-
 $('#playpause').click(function (e) {
-    paused = !paused;
-    object_mover();
-    enemy_generator();
+    if (game) {
+        game.toggle_pause();
+    }
 });
 
